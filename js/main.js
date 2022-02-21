004f166b4ed7de4762ff25c8f2d262334f956f24
Enzo Juan Andrés Almada <99050038+Lenz-or@users.noreply.github.com>
2022-02-21 19:29:18 -0300

    Agrega actualizacion de la tarea de la clase 8
    Se coloca las funciones y sus elementos del video

---
+++ js/main.js
@@ -0,0 +1,101 @@
+function validarFormulario(event){
+    const $form = document.querySelector('#carta-a-santa');
+
+    const nombre = $form.nombre.value;
+    const ciudad = $form.ciudad.value;
+    const descripcionRegalo = $form['descripcion-regalo'].value;
+
+    const errorNombre= validarNombre(nombre);
+    const errorCiudad = validarCiudad(ciudad);
+    const errorDescripcionRegalo = validarDescripcion(descripcionRegalo)
+
+    const errores = {
+        nombre: errorNombre,
+        ciudad: errorCiudad,
+        'descripcion-regalo': errorDescripcionRegalo,
+
+    };
+    
+    const esExito= manejarErrores(errores) === 0;
+
+    if(esExito){
+        $form.className= "oculto";
+        document.querySelector('#exito').className = "";
+    }
+    
+    event.preventDefault();
+
+    
+}
+
+function manejarErrores(errores){
+    
+    const keys = Object.keys(errores);
+    const $errores = document.querySelector('#errores');
+    let cantidadErrores = 0;
+
+    keys.forEach(function(key){
+        const error= (errores[key]);
+
+        if(error){
+            cantidadErrores++;
+            $form[key].className = "error";
+
+            const $error = document.createElement('li');
+            $error.innerText = error;
+            $errores.appendChild($error);
+        }else{
+            $form[key].className = "";
+        }
+
+        
+    })
+    return cantidadErrores;
+    
+}
+
+
+
+function validarNombre(nombre){
+    if(nombre.length === 0){
+        return 'Este campo debe tener al menos 1 caracter';
+    }
+    if(nombre.length >= 50){
+        return 'Este campo debe tener menos de 50 caracteres'
+    }
+    if(!/^[a-z]+$/i.test(nombre)){
+        return 'El campo nombre solo acepta letras';
+    }
+    return '';
+}
+
+function validarCiudad(ciudad){
+    if(ciudad.length === 0){
+        return 'Este campo debe contener una opcion'
+    }
+
+    return '';
+
+}
+
+function validarComportamiento(comportamiento){
+    if(comportamiento.length === 0){
+        return 'Este campo debe contener una de las tres opciones'
+    }
+    return '';
+}
+
+function validarDescripcion(descripcion){
+    if(descripcion.length === 0){
+        return 'Este campo descripcion debe tener algun caracter';
+    }
+    if(descripcion.length >= 100){
+        return 'Este campo descripcion debe tener menos de 100 caracteres'
+    }
+    return '';
+
+
+}
+
+const $form = document.querySelector('#carta-a-santa');
+$form.onsubmit = validarFormulario;
--- js/pruebas.js
+++ js/pruebas.js
@@ -1,4 +1,4 @@
-function probarValidarNombre() {
+/* function probarValidarNombre() {
   console.assert(
       validarNombre('') === 'Este campo debe tener al menos 1 caracter',
       'Validar nombre no validó que el nombre no sea vacío',
@@ -13,3 +13,59 @@ function probarValidarNombre() {
 }
 
 probarValidarNombre();
+*/
+
+function probarValidarNombre(){
+    console.assert(validarNombre('') === 'Este campo debe tener al menos 1 caracter',
+    'Validar nombre no funciona con String vacio'   , 
+    )
+    
+    console.assert(validarNombre('111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111') === 'Este campo debe tener menos de 50 caracteres',
+    'Validar nombre no funciona con mas de 50 caracteres' ,   
+    )
+
+    console.assert(validarNombre('Enzo') === '',
+    'Validar nombre fallo con un nombre valido',
+    )
+    console.assert(validarNombre('asDasd12414') === 'El campo nombre solo acepta letras',
+    'Validar nombre no valido que el nombre solo tenga letras'
+    ) 
+
+}
+
+function probarValidarCiudad(){
+    console.assert(validarCiudad('') === 'Este campo debe contener una opcion', 
+    'Validar ciudad no funciona sin elegir una opcion',
+    )
+
+    console.assert(validarCiudad('Buenos aires') === '',
+    'Validar ciudad fallo con una ciudad valida'            
+    )
+
+}
+
+function probarValidarComportamiento(){
+    console.assert(validarComportamiento('') === 'Este campo debe contener una de las tres opciones',
+    'Validar comporamiento no funciona sin colocar una opcion',
+    )
+}
+
+function probarValidarDescripcion(){
+    console.assert(validarDescripcion('') === 'Este campo descripcion debe tener algun caracter',
+    'Validar descripcion esta recibiendo el area de texto vacia',
+    )
+
+    console.assert(validarDescripcion('11111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111') === 'Este campo descripcion debe tener menos de 100 caracteres',
+    'Validar descripcion esta recibiendo mas de 100 caracteres',
+    )
+
+    console.assert(validarDescripcion('Hola') === '',
+    'Validar descripcion esta fallando con un texto valido',
+    )
+}
+
+
+probarValidarNombre();  
+probarValidarCiudad();
+probarValidarComportamiento();
+probarValidarDescripcion();
